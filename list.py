6b9b4edcee0394239ecd675534cc85592cf84758
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-12 14:36:06 -0400

    (add) Lista de modelos disponiveis de acordo com a sua API KEY

---
+++ list.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+API_KEY=os.getenv("API_GEMINI_KEY")
+genai.configure(api_key="")
+
+print("Modelos disponíveis para você:")
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
