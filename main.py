5be4fd019ebc3403463e7c273d947a28fe18145f
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-12 14:37:26 -0400

    (add) Nova main.py da aplicação, adicionando logica de carregar o arquivo .pdf para scan OCR do Gemini arquivos ≲ 20 MB

--- main.py
+++ main.py
@@ -1,61 +1,72 @@
-from doctr.io import DocumentFile
-from doctr.models import db_resnet50
-import re
-import os
-from pathlib import Path
+from google import *
 from dotenv import load_dotenv
+import google.generativeai as genai
+import time
+import json
+import os
+import re
+from pathlib import Path
 load_dotenv()
 
-def parseia_string(result):
-    nome , data, numero = None, None, None
-    linhas = []
-
-    for linha in result.splitlines():
-        if len(linhas) < 25:
-            if linha.strip():
-                linhas.append(linha.strip())
-
-    for i, linha in enumerate(linhas):
-        if nome is None and i > 0 and any(p in linhas[i - 2].upper() for p in PALAVRAS_CHAVE_NOME):
-            nome = linha
-            continue
-
-        if data is None and _data.search(linha):
-            data = _data.search(linha).group()
-            continue
-
-        if numero is None and _numero.match(linha) and i > 0 and any(p in linhas[i - 2].upper() for p in PALAVRAS_CHAVE_NUMERO) or numero is None and _numero.match(linha) and i > 0 and any(p in linhas[i - 3].upper() for p in PALAVRAS_CHAVE_NUMERO):
-            numero = linha
-            continue
-    
-    nome = nome.replace("/", ".") if nome else "Nao_encontrado"
-    data =  data.replace("/", ".") if data else "Nao_encontrado"
-    numero = numero if numero else "Nao_encontrado"
-    novo_nome=f"PGTO_N° {numero} - [{data}] - ({nome}).pdf"
-
-    return novo_nome
-
+# --- VARIAVEIS DE AMBIENTE ---
+GOOGLE_API_KEY = os.getenv("API_GEMINI_KEY")
+MODEL = os.getenv("MODEL")
 INPUT = Path(os.getenv("INPUT_PATH"))
 OUTPUT = Path(os.getenv("OUTPUT_PATH"))
 
-# MODELO DE PREDICÇÂO
-predictor = db_resnet50(pretrained=True)
-lista_arquivos = list(INPUT.glob("*.pdf"))
-PALAVRAS_CHAVE_NUMERO = ["NUMERO", "NÚMERO"]
-PALAVRAS_CHAVE_NOME = ["Razao Social/Fornecedor","Razao Social.Fornecedor","FORNECEDOR", "RAZAO SOCIAL"]
-_data = re.compile(r"\b\d{2}/\d{2}/\d{4}\b")
-_numero = re.compile(r"^\d+$")
+genai.configure(api_key=GOOGLE_API_KEY)
 
-for arq in lista_arquivos:
-    nome_arquivo = arq.name
-    doc = DocumentFile.from_pdf(arq)
-    resul = predictor(doc[:1])
-    string_result = resul.render()
-    novo_nome = parseia_string(string_result)
-    _arquivo = arq.with_name(novo_nome)
-    arq.rename(_arquivo)
-    print(f"{nome_arquivo}  -->  {novo_nome}")
+def limpar_nome_arquivo(texto):
+    """
+    Remove caracteres que o Windows/Linux/macOS não permitem em nomes de arquivos.
+    """
+    nome = texto.strip().replace('\n', ' ').replace('\r', '')
+    nome = re.sub(r'[\\/*?:"<>|]', '_', nome)
+    return nome
+
+def gemini_parse_pdf(file):
+    print(f"Fazendo upload do arquivo: {file}...")
+    sample_file = genai.upload_file(path=file, display_name="Documento pdf para OCR")
+
+    # 3. Aguardar o processamento do arquivo pelo Google
+    while sample_file.state.name == "PROCESSING":
+        print(".", end="", flush=True)
+        time.sleep(2)
+        sample_file = genai.get_file(sample_file.name)
+
+    if sample_file.state.name == "FAILED":
+        raise Exception("Falha no processamento do arquivo.")
+
+    model = genai.GenerativeModel(model_name=MODEL)
     
-    if "nao_encontrado" in _arquivo.name.lower():
-        continue  
-    _arquivo.rename(OUTPUT / _arquivo.name)
+    prompt = """
+    Analise este documento PDF. 
+    1. Realize o OCR de todo o texto visível.
+    2. Extraia os dados principais (Razão social/Fornecedor, Data do cabeçalho, Número de documento).
+    3. Se os itens do item 2. foram extraidos com sucesso, despreze o restante dos dados do documento.
+    4. Formate a saida de dados para que seja respondido somente "PGTO_N° <Número de documento> - [<Data do cabeçalho>] - (<Razão social/Fornecedor>).pdf"
+    5. Substitua todos os caracteres ("/, \, :, *, ?, ", <, >, |, \n") presentes na saida formata por "." (ponto).
+    6. Responda somente a saida formatada
+    """
+    
+    response = model.generate_content([sample_file, prompt])
+    return response.text
+
+# --- Execução ---
+if __name__ == "__main__":
+    
+    # PASTA DE ENTRADA E SAIDA DE ARQUIVOS .pdf
+    arquivos = list(INPUT.glob("*.pdf"))
+    
+    for arq in arquivos:
+        try:
+            nome_arquivo = arq.name
+            result = gemini_parse_pdf(arq)
+            novo_nome = limpar_nome_arquivo(result)
+            _arquivo = arq.with_name(novo_nome)
+            arq.rename(_arquivo)
+            print(f"\n {nome_arquivo}  -->  {novo_nome}")
+            _arquivo.rename(OUTPUT / _arquivo.name)
+                
+        except Exception as e:
+            print(f"\nErro: {e}")
